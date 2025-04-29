c2835a428619441a771c697ce5cc8f6038d3597e
1JOPL <2801324391@qq.com>
2025-04-29 17:56:20 +0800

    Update good.py

--- good.py
+++ good.py
@@ -4,5 +4,9 @@ print("hello shrimp")
 <<<<<<< Updated upstream
 =======
 print("hello 你好")
+<<<<<<< Updated upstream
+print("hello 你好")
+>>>>>>> Stashed changes
+=======
 print("hello 你好")
 >>>>>>> Stashed changes
